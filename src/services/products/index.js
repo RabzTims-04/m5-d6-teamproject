7b357c1f56ac8d5eb20f091178433305861c7184
Rabz92 <rabbiailyas92@gmail.com>
2021-06-28 13:35:46 +0200

    reviews Done

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -3,6 +3,7 @@ import uniqid from 'uniqid'
 import createError from 'http-errors'
 import multer from "multer";
 import { extname } from "path"
+import { validationResult } from 'express-validator'
 import { getProducts, writeProducts, writeProductsPicture } from "../../lib/fs-tools.js";
 import { checkSearchSchema, checkValidationResult } from "./validation.js";
 
@@ -63,13 +64,126 @@ productsRouter.post('/:id/upload',multer().single("image"),async (req,res, next)
 /* *********************REVIEWS************************* */
 
 /* GET all reviews */
+productsRouter.get("/:id/reviews", async (req, res, next) => {
+    try {
+        const products = await getProducts()
+        const product = products.find(product=> product._id === req.params.id)
+        if(product){
+            const productReview = product.reviews
+            res.send(productReview)
+        }
+        else{
+            next(createError(404, `Product with id: ${req.params.id} not found`))
+        }
+
+    } catch (error) {
+        next(error)
+    }
+})
 
 /* GET single review */
+productsRouter.get("/:id/reviews/:revId", async (req, res, next) => {
+    try {
+        const products = await getProducts()
+        const product = products.find(product => product._id === req.params.id)
+        if(product){
+            const productReviews = product.reviews
+            if(productReviews){
+                const singleReview = productReviews.find(review => review._id === req.params.revId)
+                res.send(singleReview)
+            }
+        }
+        else{
+            next(createError(404, `Product with id: ${req.params.id} or review with id: ${req.params.revId} not found`))
+        }
+    } catch (error) {
+        next(error)
+    }
+})
 
 /* POST a review */
 
+productsRouter.post("/:id/reviews", async (req, res, next) => {
+    try {
+        const errors = validationResult(req)
+
+        if(errors.isEmpty()){
+            const products = await getProducts()
+            const product = products.find(product => product._id === req.params.id)
+            if(product){
+                const productReviews = product.reviews
+                const newReview = {
+                    ...req.body,
+                    _id: uniqid(),
+                    createdAt: new Date()
+                }
+                productReviews.push(newReview)
+                await writeProducts(products)
+                res.status(201).send({_id: newReview._id})
+            }
+        }
+        else{
+            next(createError(400, {errorList:errors}))
+        }
+
+    } catch (error) {
+        next(error)
+    }
+})
+
 /* PUT a review */
+productsRouter.put('/:id/reviews/:revId',async (req,res, next)=>{
+    
+    try {
+            const products = await getProducts()
+            const productIndex = products.findIndex(product=> product._id === req.params.id)
+            console.log("index", productIndex);
+            if(productIndex !== -1){
+                let product = products[productIndex]
+                let productReviews = product.reviews
+                console.log('productReviews',productReviews);
+                let ProductReviewIndex = productReviews.findIndex(review => review._id === req.params.revId)
+                console.log('ProductReviewIndex', ProductReviewIndex);
+                let editReview = productReviews[ProductReviewIndex]
+                console.log('editReview', editReview);
+                console.log('Reviewbody', req.body);
+                editReview ={
+                    ...editReview,
+                    ...req.body,
+                    _id:req.params.revId,
+                    updatedAt: new Date()
+                }
+                console.log('editReview', editReview);
+                
+                productReviews[ProductReviewIndex] = editReview
+                await writeProducts(products)
+                res.send(product) 
+            }
+            else{
+                next(createError(400, {errorsList: "error"}))
+            }        
+    }catch (error) {
+        next(error)
+    }
+})
 
 /* DELETE a review */
+productsRouter.delete('/:id/reviews/:revId',async (req,res, next)=>{
+
+    try {
+        const products = await getProducts()
+        const product = products.find(product => product._id === req.params.id)
+        if(product){
+            const productReviews = product.reviews
+            const deleteReview = productReviews.findIndex(review => review._id === req.params.revId)
+            res.send(productReviews[deleteReview])
+            productReviews.splice(deleteReview,1)
+            await writeProducts(products)
+            res.status(200)      
+        }
+    } catch (error) {
+        next(error)
+    }
+})
 
 export default productsRouter
